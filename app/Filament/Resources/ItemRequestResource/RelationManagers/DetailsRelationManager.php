e3e791de5125ee2ae724be7669b15e67ce330bcc
warih_suryono <warih_suryono@yahoo.com>
2025-06-25 17:04:36 +0700

    storage location pada request item view & berwarna merah jika stock kurang

--- app/Filament/Resources/ItemRequestResource/RelationManagers/DetailsRelationManager.php
+++ app/Filament/Resources/ItemRequestResource/RelationManagers/DetailsRelationManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\ItemRequestResource\RelationManagers;
 
+use App\Models\FollowupOfficer;
 use Filament\Forms;
 use App\Models\Item;
 use App\Models\Unit;
@@ -10,9 +11,11 @@ use Filament\Forms\Get;
 use Filament\Forms\Set;
 use Livewire\Component;
 use Filament\Forms\Form;
+use App\Models\ItemStock;
 use Filament\Tables\Table;
 use App\Models\WarehouseDetail;
 use App\Models\ItemRequestDetail;
+use Illuminate\Support\Facades\Auth;
 use Filament\Forms\Components\Select;
 use App\Tables\Columns\SelectCheckbox;
 use Filament\Forms\Components\TextInput;
@@ -59,14 +62,34 @@ class DetailsRelationManager extends RelationManager
 
     public function table(Table $table): Table
     {
+        $is_stock_visible = false;
+        if (Auth::user()->privilege->id == 1) $is_stock_visible = true;
+        if (@FollowupOfficer::where(['user_id' => Auth::user()->id, 'action' => 'item-request-issue'])->first()->id > 0) $is_stock_visible = true;
+        if (@FollowupOfficer::where(['user_id' => Auth::user()->id, 'action' => 'stock-keeper'])->first()->id > 0) $is_stock_visible = true;
+
         return $table
             ->recordTitleAttribute('item_id')
             ->columns([
                 Tables\Columns\TextColumn::make('item_request_type.name')->label('Request Type'),
-                Tables\Columns\TextColumn::make('item.name'),
-                Tables\Columns\TextColumn::make('qty'),
+                Tables\Columns\TextColumn::make('item_id')->label('Item')
+                    ->formatStateUsing(fn($state) => "[" . Item::find($state)->code . "] -- " . Item::find($state)->name),
+                Tables\Columns\TextColumn::make('qty')->alignRight(),
+                Tables\Columns\TextColumn::make('stock')->default(fn($record) => ItemStock::find($record->item_id)->qty ?? 0)->alignRight()
+                    ->color(fn($record) => (@ItemStock::find($record->item_id)->qty < $record->qty) ? 'danger' : 'primary')
+                    ->visible($is_stock_visible)
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('unit.name'),
                 Tables\Columns\TextColumn::make('notes'),
+                Tables\Columns\TextColumn::make('warehouse_detail_ids')->label('Storage Locations')->default(function ($record) {
+                    $warehouse_detail_ids = json_decode(Item::find($record->item_id)->warehouse_detail_ids);
+                    if ($warehouse_detail_ids) {
+                        $warehouse_details = "";
+                        foreach ($warehouse_detail_ids as $warehouse_detail_id) {
+                            $warehouse_details .= WarehouseDetail::find($warehouse_detail_id)->code . ", ";
+                        }
+                        return substr($warehouse_details, 0, -2);
+                    } else return '';
+                })->visible($is_stock_visible)->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //

990ddac67899011bd2161a16ecf37f3f4af53c99
warih_suryono <warih_suryono@yahoo.com>
2025-08-15 09:33:10 +0700

    fix bug add privileges

--- database/migrations/2025_08_11_161918_add_privileges_urgent_work_order.php
+++ database/migrations/2025_08_11_161918_add_privileges_urgent_work_order.php
@@ -12,11 +12,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $menu_id = DB::table('menus')->where('url', 'urgent-work-orders')->first()->id;
-        for ($privilege_id = 2; $privilege_id <= 7; $privilege_id++) {
-            $menu_ids = DB::table('privileges')->where('id', $privilege_id)->first()->menu_ids . ',' . ($menu_id);
-            $privileges = DB::table('privileges')->where('id', $privilege_id)->first()->privileges . ',15';
-            DB::table('privileges')->where('id', $privilege_id)->update(['menu_ids' => $menu_ids, 'privileges' => $privileges]);
+        try {
+            $menu_id = DB::table('menus')->where('url', 'urgent-work-orders')->first()->id;
+            for ($privilege_id = 2; $privilege_id <= 7; $privilege_id++) {
+                $menu_ids = DB::table('privileges')->where('id', $privilege_id)->first()->menu_ids . ',' . ($menu_id);
+                $privileges = DB::table('privileges')->where('id', $privilege_id)->first()->privileges . ',15';
+                DB::table('privileges')->where('id', $privilege_id)->update(['menu_ids' => $menu_ids, 'privileges' => $privileges]);
+            }
+        } catch (\Exception $e) {
+            // Handle exception
         }
     }
 
